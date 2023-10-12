2e3a7fe0204cb4b9cddb3e3018ce19d794f0ef1f
Janak singh dhami <info@jsdhami.com.np>
2023-10-12 15:24:56 +0545

    basic update

---
+++ Practice/first.rb
@@ -0,0 +1,29 @@
+# Integer variable
+num = 10
+
+# Float variable
+float_num = 3.14
+
+# String variable
+name = "John Doe"
+
+# Boolean variable
+is_true = true
+
+# Array variable
+arr = [1, 2, 3, 4, 5]
+
+# Hash variable
+hash = { "name" => "John", "age" => 30, "city" => "New York" }
+
+# Nil variable
+nil_var = nil
+
+# Output all variables
+puts "Integer variable: #{num}"
+puts "Float variable: #{float_num}"
+puts "String variable: #{name}"
+puts "Boolean variable: #{is_true}"
+puts "Array variable: #{arr}"
+puts "Hash variable: #{hash}"
+puts "Nil variable: #{nil_var}"
