29880bbb73fd53fe28ea462c0f2736d5385885d7
Janak singh dhami <info@jsdhami.com.np>
2023-10-12 16:00:05 +0545

    update basic programs

---
+++ Practice/Comments.rb
@@ -0,0 +1,4 @@
+# this is a single line comments in ruby
+=begin
+this is a multi line comments in ruby
+=end
---
+++ Practice/HelloWorld.rb
@@ -0,0 +1,8 @@
+# this is a basic program for hello world
+puts 'Hello WOrld'
+puts "Hello world"
+# the puts command helps us to pring output
+
+a = 6
+puts a
+# the above code will print the value of a
